aa7ac4cdc96ef3760aa7d544a0a8f56dd4c04800
prossew <snowniksa@gmail.com>
2026-03-11 01:21:59 +0300

    feat: timeframe switcher 1D 1W 1M

--- src/pages/Coin.tsx
+++ src/pages/Coin.tsx
@@ -5,6 +5,7 @@ import Chart from "../widgets/Chart";
 function Coin() {
   const { id } = useParams();
   const [coin, setCoin] = useState<any>(null);
+  const [days, setDays] = useState(7);
 
   useEffect(() => {
     async function fetchCoin() {
@@ -14,9 +15,14 @@ function Coin() {
     fetchCoin();
   }, [id]);
   if (!coin) return <div>Loading ...</div>;
+
   return (
     <div>
-      <h1>{<Chart id={id!} days={7} />}</h1>
+      <h1>{coin.name}</h1> 
+      <Chart id={id!} days={days} />
+      <button onClick={() => setDays(1)}>1D</button>
+      <button onClick={() => setDays(7)}>1W</button>
+      <button onClick={() => setDays(30)}>1M</button>
     </div>
   );
 }
