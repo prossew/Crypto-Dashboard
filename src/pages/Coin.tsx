69a23a9d3fb1389ebd32d29bf6ef124eb90a2584
prossew <snowniksa@gmail.com>
2026-03-11 00:28:19 +0300

    feat: coin page with data fetching

--- src/pages/Coin.tsx
+++ src/pages/Coin.tsx
@@ -1,11 +1,22 @@
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { getCoinsbyId } from "../entities/coin/api";
 
 function Coin() {
   const { id } = useParams();
+  const [coin, setCoin] = useState<any>(null);
 
+  useEffect(() => {
+  async function fetchCoin() {
+    const data = await getCoinsbyId(id!);
+    setCoin(data); 
+  }
+  fetchCoin();
+}, [id]);
+  if (!coin) return <div>Loading ...</div>;
   return (
     <div>
-      <h1>User id {id}</h1>
+      <h1>{coin.name}</h1>
     </div>
   );
 }
